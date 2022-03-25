ee9089ddfa7aa5fa809a70c8dcf4008f543f9fa9
iado <iado@foxmail.com>
2022-03-26 04:57:29 +0800

    uuuuu

---
+++ wp-content/plugins/booster-plus-for-woocommerce/includes/js/wcj-fbt-script.js
@@ -0,0 +1,8 @@
+/**
+ * wcj-fbt-script.
+ *
+ * @version 5.4.2
+ */
+
+//jQuery(document.body).on('change','.variations select',change_price);
+//jQuery(document.body).on('change','input[name="wcj_variations"]',change_price);
---
+++ wp-content/plugins/booster-plus-for-woocommerce/includes/js/wcj-opc-script.js
@@ -0,0 +1,46 @@
+/**
+ * wcj-opc-script.
+ *
+ * @version 5.4.2
+ * @since   5.4.2
+ */
+
+jQuery(document).ready(function ($) {
+
+	var opc_response_messages = '';
+
+	$('.wcj_opc_add_to_cart_btn').on('click', function (e) {
+		var product_id = $(this).val();
+		var product_title = $(this).data('product_title');
+		var data = {
+			'action': 'wcj_ajax_add_opc_add_to_cart',
+			'product_id': product_id,
+		};
+		$.ajax({
+			type: "POST",
+			url: ajax_object.ajax_url,
+			data: data,
+			dataType: 'json',
+			success: function (response) {
+				opc_response_messages = response.messages;
+				$('#wcj_opc_notices .woocommerce-error, #wcj_opc_notices .woocommerce-message, #wcj_opc_notices .woocommerce-info').remove();
+				$('body').trigger('update_checkout');
+			}
+		});
+	});
+
+	$('body').on('updated_checkout', function () {
+		if (opc_response_messages.length > 0) {
+			var $wcj_opc_notices = $('#wcj_opc_notices');
+
+			$wcj_opc_notices.prepend(opc_response_messages);
+
+			$('html, body').animate({
+				scrollTop: ($wcj_opc_notices.offset().top - 50)
+			}, 500);
+
+			opc_response_messages = '';
+		}
+	});
+
+});
