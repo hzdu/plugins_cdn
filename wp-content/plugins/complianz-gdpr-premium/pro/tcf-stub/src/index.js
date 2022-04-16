3dbc37868067f2b7639b37647ceec1b60368d6e6
iado <iado@foxmail.com>
2022-04-16 21:45:13 +0800

    add complianz-gdpr-premium 6.1.5

---
+++ wp-content/plugins/complianz-gdpr-premium/pro/tcf-stub/src/index.js
@@ -0,0 +1,9 @@
+import {CmpApi} from '@iabtcf/cmpapi';
+import {TCModel, TCString, GVL, Segment} from '@iabtcf/core';
+import * as cmpstub from '@iabtcf/stub';
+
+/**
+ * initialize the __tcfapi function and post message
+ * https://github.com/InteractiveAdvertisingBureau/iabtcf-es/tree/master/modules/stub
+ */
+cmpstub();
