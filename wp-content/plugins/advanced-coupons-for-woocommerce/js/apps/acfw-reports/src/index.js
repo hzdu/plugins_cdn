16b0affb8d15013a25c1999ce5100661a0009725
iado <iado@foxmail.com>
2022-05-16 14:15:50 +0800

    update ajax search pro to 4.22

--- wp-content/plugins/advanced-coupons-for-woocommerce/js/apps/acfw-reports/src/index.js
+++
@@ -1,8 +0,0 @@
-/* global jQuery */
-import "./assets/styles/index.scss";
-import { orders_with_coupons_init } from "./orders_with_coupons";
-
-jQuery( document ).ready( ($) => {
-
-    orders_with_coupons_init( $ );
-} );
--- wp-content/plugins/advanced-coupons-for-woocommerce/js/apps/loyalprog-my-account/src/index.js
+++
@@ -1,8 +0,0 @@
-/* global jQuery */
-import "./assets/styles/index.scss";
-import { my_account_events } from "./my_account";
-
-jQuery( document ).ready( function($) {
-
-    my_account_events($);
-});
