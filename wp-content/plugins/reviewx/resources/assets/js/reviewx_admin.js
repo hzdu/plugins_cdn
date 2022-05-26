d7eee63ff82e9f63f06332cb33e6e2eb551ae038
iado <iado@foxmail.com>
2022-05-26 16:07:32 +0800

    update reviewx pro

---
+++ wp-content/plugins/reviewx/resources/assets/js/reviewx_admin.js
@@ -0,0 +1,29 @@
+(function( $ ) {
+    let carLmt = 50;
+    let readMoreTxt = " ... More";
+    let readLessTxt = "Less";
+
+    $(".review_comments").each(function() {
+
+        let allstr = $(this).text();
+        if (allstr.length > carLmt) {
+            let firstSet = allstr.substring(0, carLmt);
+            let secdHalf = allstr.substring(carLmt, allstr.length);
+            let strtoadd = firstSet + "<span class='SecSec'>" + secdHalf + "</span><span class='readMore'  title='Click to Show More'>" + readMoreTxt + "</span><span class='readLess' title='Click to Show Less'>" + readLessTxt + "</span>";
+            $(this).html(strtoadd);
+        }
+
+    });
+
+    $(document).on("click", ".readMore,.readLess", function() {
+        $(this).closest(".review_comments ").toggleClass("column-review_comments showmorecontent");
+    });
+
+    //single product review id and from submit
+    $(document).on("click", ".rx-single-product", function() {
+        let productId = $(this).attr("data-id");
+        $(".rx-product-id-input").val(productId);
+        $(".rx-all-reviews-from").submit();
+    });
+
+})( jQuery );
