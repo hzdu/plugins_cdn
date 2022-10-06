803d22d8d643784e18bd63874e9ec483279334d0
hzdu <iado@foxmail.com>
2022-10-07 04:03:55 +0800

    update: wordpress automatic to 3.58.0

--- wp-content/plugins/wp-automatic/js/main.js
+++ wp-content/plugins/wp-automatic/js/main.js
@@ -629,7 +629,7 @@ jQuery(document).ready(function()
     	    	
     	     	jQuery('#campaign_run').addClass('run').removeClass('run_load');
 
-	    	    	jQuery('#welcome-panel').remove();
+	    	    	jQuery('#wp-automatic-welcome-panel').remove();
 	    	    	jQuery('#status-meta-boxes .inside').append('<div style="display:none;margin-top:0;padding-top:10px;" dir="ltr" class="wp-automatic-welcome-panel" id="wp-automatic-welcome-panel"> '+  two + ' ' +  three + ' ' + four +  ' <a dir="rtl" style="right:10px !important" href="#" class="wp-automatic-welcome-panel-close">Dismiss</a></div>');
 	    	    	jQuery('#wp-automatic-welcome-panel').slideDown('slow',function(){
 	    	    		
