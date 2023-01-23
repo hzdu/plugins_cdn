467b3e5bf05adafbb4d712c3c6009d144b6ea0b0
hzdu <iado@foxmail.com>
2023-01-23 21:06:19 +0800

    update

---
+++ wp-content/plugins/novashare/js/blocks-follow.js
@@ -0,0 +1,436 @@
+(function(blocks, element, blockEditor, components) {
+
+	//setup constants
+	const el = element.createElement;
+	const { registerBlockType } = blocks; 
+	const { InspectorControls, InnerBlocks, useBlockProps, useInnerBlocksProps, PanelColorSettings } = blockEditor;
+	const { Fragment, useState } = element;
+	const { TextControl, ToggleControl, Panel, PanelBody, PanelRow, ColorPicker, Icon, RangeControl, Button, ButtonGroup, BaseControl, Popover, __experimentalInputControl } = components;
+	const { __ } = wp.i18n;
+
+	//swap button colors on hover in editor
+	const hoverSwap = (event) => {
+		var wrapper = event.target.closest('.novashare-buttons-wrapper');
+		if(wrapper) {
+			var buttonHoverColor = wrapper.getAttribute('data-button-hover-color');
+		 	var iconHoverColor = wrapper.getAttribute('data-icon-hover-color');
+		 	var blocks = event.target.querySelectorAll('.novashare-button-wrapper .novashare-button-block');
+
+		 	blocks.forEach(function(block) {
+		 		if(event.type == 'mouseover') {
+		 			block.setAttribute('data-button-color', block.style.backgroundColor);
+		 			block.setAttribute('data-icon-color', block.style.color);
+		 			block.style.backgroundColor = buttonHoverColor || block.style.backgroundColor;
+		 			block.style.color = iconHoverColor || block.style.color;
+		 		}
+		 		else if(event.type == 'mouseout') {
+		 			block.style.backgroundColor = block.getAttribute('data-button-color');
+		 			block.style.color = block.getAttribute('data-icon-color');
+		 		}
+		 	});
+		}
+	}
+ 
+ 	//register follow block
+	registerBlockType('novashare/follow', {
+		apiVersion: 2,
+		title: __("Follow Buttons", 'novashare'),
+		description: __("Add follow buttons for your social network profiles.", 'novashare'),
+		category: 'widgets',
+		icon: 'share-alt2',
+		keywords: ["follow", "buttons", "novashare"],
+		supports: {
+        	html: false
+        },
+		attributes: {
+			id: {
+				type: 'string'
+			},
+			buttonShape: {
+				type: 'string'
+			},
+			alignment: {
+				type: 'string'
+			},
+			buttonSize: {
+				type: 'number'
+			},
+			buttonMargin: {
+				type: 'number'
+			},
+			newTab: {
+				type: 'boolean',
+			},
+			buttonColor: {
+				type: 'string'
+			},
+			iconColor: {
+				type: 'string'
+			},
+			buttonHoverColor: {
+				type: 'string'
+			},
+			iconHoverColor: {
+				type: 'string'
+			},
+		},
+		providesContext: {
+        	'buttonShape': 'buttonShape',
+        	'buttonSize': 'buttonSize',
+        	'newTab': 'newTab',
+        	'buttonColor': 'buttonColor',
+        	'iconColor': 'iconColor',
+        	'buttonHoverColor': 'buttonHoverColor',
+        	'iconHoverColor': 'iconHoverColor',
+    	},
+
+    	//add hover data attributes to wrapper
+    	getEditWrapperProps(attributes) {
+	    	return {
+	      		'data-button-hover-color': attributes.buttonHoverColor,
+		      	'data-icon-hover-color': attributes.iconHoverColor
+		    };
+		},
+
+		edit: (function(props) {
+
+			props.setAttributes({
+	  			id: props.clientId
+	  		});
+
+			const blockProps = useBlockProps({
+				className: 'novashare-buttons-wrapper' + (props.attributes.alignment ? ' novashare-align-' + props.attributes.alignment : ''),
+				style: {
+					gap: (props.attributes.buttonMargin ?? 10) + 'px'
+				}
+			});
+
+			const followPlaceholder = el('div', {
+				style: {
+					display: 'flex',
+					alignItems: 'center'
+				}},
+				el(Icon, {
+					icon: 'share-alt2', 
+					style: {
+						marginRight: '5px'
+					}
+				}),
+				__('Follow Buttons', 'novashare')
+			);
+
+  			const selectedFollowPlaceholder = el("span", {}, __('Add Networks', 'novashare')); 
+
+	        const innerBlocksProps = useInnerBlocksProps(blockProps, {
+	            allowedBlocks: ['novashare/follow-network'],
+	            orientation: 'horizontal',
+	            placeholder: props.isSelected ? selectedFollowPlaceholder : followPlaceholder,
+	        });
+
+			return (
+				el(Fragment, {},
+
+					//sidebar block controls
+					el(InspectorControls, {},
+						el(PanelBody, { title: __('Settings', 'novashare'), className: 'novashare-block-settings', initialOpen: true },
+
+							//button shape
+							el(BaseControl, { label: __('Button Shape', 'novashare') },
+								el(ButtonGroup, { style: { display: 'flex' } },
+									el(Button, {
+										variant: 'secondary', 
+										isPressed: (() => { return (props.attributes.buttonShape == '' ? true : false) })(),
+										onClick: (event) => { props.setAttributes({ buttonShape: event.target.value }) },
+										value: '',
+										style: { display: 'inline-flex', flexGrow: '1', justifyContent: 'center' }
+									},__('Squared', 'novashare')),
+									el(Button, {
+										variant: 'secondary', 
+										isPressed: (() => { return (props.attributes.buttonShape == 'rounded' ? true : false) })(),
+										onClick: (event) => { props.setAttributes({ buttonShape: event.target.value }) },
+										value: 'rounded',
+										style: { display: 'inline-flex', flexGrow: '1', justifyContent: 'center' }
+									}, __('Rounded', 'novashare')),
+									el(Button, {
+										variant: 'secondary', 
+										isPressed: (() => { return (props.attributes.buttonShape == 'circular' ? true : false) })(),
+										onClick: (event) => { props.setAttributes({ buttonShape: event.target.value }) },
+										value: 'circular',
+										style: { display: 'inline-flex', flexGrow: '1', justifyContent: 'center' }
+									}, __('Circular', 'novashare'))
+								)
+							),
+
+							//alignment
+							el(BaseControl, { label: __('Alignment', 'novashare') },
+								el(ButtonGroup, { style: { display: 'flex'} },
+									el(Button, {
+										variant: 'secondary', 
+										isPressed: (() => { return (props.attributes.alignment == '' ? true : false) })(),
+										onClick: (event) => { props.setAttributes({ alignment: event.target.value }) },
+										value: '',
+										icon: el(Icon, { icon: 'align-left', style: { pointerEvents: 'none' } }),
+										style: { display: 'inline-flex', flexGrow: '1' }
+									}),
+									el(Button, {
+										variant: 'secondary', 
+										isPressed: (() => { return (props.attributes.alignment == 'center' ? true : false) })(),
+										onClick: (event) => { props.setAttributes({ alignment: event.target.value }) },
+										value: 'center',
+										icon: el(Icon, { icon: 'align-center', style: { pointerEvents: 'none' } }),
+										style: { display: 'inline-flex', flexGrow: '1' }
+									}),
+									el(Button, {
+										variant: 'secondary', 
+										isPressed: (() => { return (props.attributes.alignment == 'right' ? true : false) })(),
+										onClick: (event) => { props.setAttributes({ alignment: event.target.value }) },
+										value: 'right',
+										icon: el(Icon, { icon: 'align-right', style: { pointerEvents: 'none' } }),
+										style: { display: 'inline-flex', flexGrow: '1' }
+									})
+								)
+							),
+
+							//button size
+							el(RangeControl, {
+								label: __('Button Size', 'novashare'),
+								onChange: (value) => {
+									props.setAttributes({ buttonSize: value });
+								},
+								value: props.attributes.buttonSize,
+								min: 10,
+								max: 100,
+								initialPosition: 50
+							}),
+
+							//button margin
+							el(RangeControl, {
+								label: __('Button Margin', 'novashare'),
+								onChange: (value) => {
+									props.setAttributes({ buttonMargin: value });
+								},
+								value: props.attributes.buttonMargin,
+								min: 0,
+								max: 20,
+								initialPosition: 10
+							}),
+
+							//new tab
+							el(ToggleControl, {
+								label: __('Open Links in New Tab', 'novashare'),
+								onChange: (value) => {
+									props.setAttributes({ newTab: value });
+								},
+								checked: props.attributes.newTab
+							}),
+						),
+
+						//colors
+						el(PanelColorSettings, {
+							title: __('Colors', 'novashare'),
+							colorSettings: [
+								{
+									value: props.attributes.buttonColor,
+									onChange: (color) => props.setAttributes({ buttonColor: color }),
+									label: __('Button Color', 'novashare')
+								},
+								{
+									value: props.attributes.buttonHoverColor,
+									onChange: (color) => props.setAttributes({ buttonHoverColor: color }),
+									label: __('Button Hover Color', 'novashare')
+								},
+								{
+									value: props.attributes.iconColor,
+									onChange: (color) => props.setAttributes({ iconColor: color }),
+									label: __('Icon Color', 'novashare')
+								},
+								{
+									value: props.attributes.iconHoverColor,
+									onChange: (color) => props.setAttributes({ iconHoverColor: color }),
+									label: __('Icon Hover Color', 'novashare')
+								}
+							]
+						}),
+					),
+
+					//print block in editor
+					el('div', { className: 'novashare-buttons novashare-block-' + props.attributes.id, style: { width: '100%' } },
+						el("div", innerBlocksProps, innerBlocksProps.children)
+					),
+				)
+			);
+		}),
+
+		//save block
+		save: function(props) {
+            return (
+            	el("div", {className: 'novashare-buttons novashare-block-' + props.attributes.id},
+            		el("div", useInnerBlocksProps.save(useBlockProps.save({className: 'novashare-buttons-wrapper'  + (props.attributes.alignment ? ' novashare-align-' + props.attributes.alignment : '')})))
+            	)
+            );
+		}
+	});
+
+	var variations = [];
+	var networkIcons = [];
+
+	//setup variations
+	Object.keys(novashare.networks.follow).forEach(function(key) {
+		const network = novashare.networks.follow[key];
+		var iconParts = network.icon.match(/viewBox="(.*?)".*?d="(.*?)"/);
+        var networkIcon = el('svg', { 
+				fill: 'currentColor',
+				viewBox: iconParts[1]
+			},
+			el( 'path', { 
+				d: iconParts[2]
+				}
+			)
+		);
+
+    	variations.push({
+			isDefault: key === 'facebook' ? true : false,
+        	name: key,
+			title: network.name,
+			icon: networkIcon,
+		    attributes: {
+		    	network: key,
+		    	icon: networkIcon
+		    }
+	    });
+
+	    networkIcons[key] = networkIcon;
+	});
+
+	variations.forEach((variation) => {
+		if(variation.isActive) {
+			return;
+		} 
+		variation.isActive = (blockAttributes, variationAttributes) => blockAttributes.network === variationAttributes.network;
+	});
+
+	//register follow network block
+	registerBlockType('novashare/follow-network', {
+		apiVersion: 2,
+	 	title: __('Follow Network', 'novashare'),
+	 	description: __('Add an icon linking to a social network profile.', 'novashare'),
+		parent: [ 'novashare/follow' ],
+		supports: { reusable: false, html: false },
+		attributes: { 
+			network: { type: "string" },
+			buttonShape: { type: "string" },
+			newTab: { type: "boolean" },
+			link: { type: "string" }
+		},
+	  	usesContext: ['buttonShape', 'buttonSize', 'newTab', 'buttonColor', 'iconColor', 'buttonHoverColor', 'iconHoverColor'],
+
+		edit: function(props) {
+
+	  		props.setAttributes({
+	  			buttonShape: props.context['buttonShape'],
+	  			newTab: props.context['newTab']
+	  		});
+
+	  		const [showPopover, setShowPopover] = useState(false);
+			const toggleVisible = () => {
+				setShowPopover(true);
+			};
+
+	  		const networkLinkPopover = () => {
+				return showPopover && el(Popover, {
+						placement: "bottom-right",
+						onClose: () => setShowPopover(false) 
+					},
+					el("form", {
+				        className: "block-editor-url-popover__link-editor",
+				        onSubmit: (e) => {
+				            e.preventDefault(), setShowPopover(false);
+				        }},
+				    	el(__experimentalInputControl, { 
+				    		isPressEnterToChange: true,
+				    		style: { width: '300px', height: '36px' },
+				    		value: props.attributes.link, 
+				    		onChange: (value) => {
+				    			props.setAttributes({ link: value });
+				    			setShowPopover(false);
+				    		}, 
+				    		placeholder: (() => {
+								switch(props.attributes.network) {
+							  		case 'email':
+								    	return 'email@example.com or https://';
+								    	break;
+								  	case 'phone':
+								    	return '(123)-456-7890';
+							   	 		break;
+							   	 	case 'messenger':
+								    	return 'mypagename';
+							   	 		break;
+						   	 		case 'skype':
+								    	return 'myusername';
+							   	 		break;
+						   	 		case 'line':
+								    	return 'mylineid';
+							   	 		break;
+						   	 		case '':
+								    	return '';
+							   	 		break;
+								  	default:
+								   		return 'https://';
+								}
+					  		})(),
+				    	}),
+					    el(Button, { 
+					    	icon: 'editor-break',
+					    	label: __("Apply", 'novashare'),
+					    	type: "submit",
+					    	onClick: () => { 
+					    		setShowPopover(false);
+					    	}
+					    })
+					)
+            	);
+			};
+
+	 		return (
+				el(Fragment, {},
+
+					//print block in editor
+					el('a', (useBlockProps)({
+						onMouseOver: hoverSwap, 
+						onMouseOut: hoverSwap,
+						onClick: () => { 
+					        toggleVisible(true);
+					    },
+						className: props.attributes.network + ' novashare-button novashare-follow-button', 
+						style: {
+							margin: '0',
+							width: props.context['buttonSize'],
+							height: props.context['buttonSize'],
+							minWidth: props.context['buttonSize']
+						}}),
+						el('span', { className: 'novashare-button-wrapper novashare-button-block' + (props.context['buttonShape'] ? ' novashare-' + props.context['buttonShape'] : '') },
+							el('span', { 
+								className: 'novashare-button-icon novashare-button-block', 
+								style: { 
+									backgroundColor: props.context['buttonColor'], 
+									color: props.context['iconColor'],
+									width: props.context['buttonSize'],
+									height: props.context['buttonSize'],
+									minWidth: props.context['buttonSize']
+								}},
+								networkIcons[props.attributes.network]
+							),
+						),
+						el(networkLinkPopover)
+					)
+				)
+			);
+	 	},
+	 	variations
+	});
+})(
+	window.wp.blocks,
+	window.wp.element,
+	window.wp.blockEditor,
+	window.wp.components,
+);
---
+++ wp-content/plugins/novashare/js/blocks.js
@@ -0,0 +1,294 @@
+(function(blocks, element, blockEditor, components, data) {
+
+	//grab plugin settings
+	const settings = novashare;
+
+	//setup constants
+	const el = element.createElement;
+	const { registerBlockType } = blocks; 
+	const { RichText, InspectorControls } = blockEditor;
+	const { Fragment } = element;
+	const { SelectControl, TextControl, ToggleControl, Panel, PanelBody, PanelRow, ColorPicker } = components;
+	const { withSelect } = wp.data;
+	const { __ } = wp.i18n;
+	const defaultAccentColor = settings.click_to_tweet.accent_color && settings.click_to_tweet.accent_color !== '' ? settings.click_to_tweet.accent_color : '#00abf0'
+
+	//twitter svg icon
+	const iconTwitter = el('svg', { 
+			width: 20, 
+			height: 20,
+			fill: 'currentColor',
+			viewBox: '0 0 512 512'
+		},
+		el( 'path', { 
+			d: "M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z" 
+			}
+		)
+	);
+ 
+ 	//register click to tweet block
+	registerBlockType('novashare/click-to-tweet', {
+		title: __("Click to Tweet", 'novashare'),
+		description: __("Add a tweet box.", 'novashare'),
+		category: 'widgets',
+		icon: iconTwitter,
+		keywords: ["Twitter", "Tweet", "Novashare"],
+
+		//UI attributes
+		attributes: {
+			theme: {
+				type: 'string'
+			},
+			tweet: {
+				type: 'string'
+			},
+			cta_text: {
+				type: 'string'
+			},
+			cta_position: {
+				type: 'string'
+			},
+			remove_url: {
+				type: 'boolean',
+				default: settings.click_to_tweet.remove_url ? true : false
+			},
+			remove_username: {
+				type: 'boolean',
+				default: settings.click_to_tweet.remove_username ? true : false
+			},
+			hide_hashtags: {
+				type: 'boolean',
+				default: settings.click_to_tweet.hide_hashtags ? true : false
+			},
+			accent_color: {
+				type: 'string',
+				default: defaultAccentColor
+			}
+		},
+
+		//react edit state
+		edit: withSelect(function(select) {
+			return {
+				permalink: select('core/editor').getPermalink()
+			};
+		})(function(props) {
+
+			//global variables
+			var count_status;
+			var chars_remaining;
+		 
+			return (
+				el(Fragment, {},
+
+					//sidebar block controls
+					el(InspectorControls, {},
+						el(PanelBody, { title: __('Settings', 'novashare'), className: 'novashare-block-settings', initialOpen: true },
+
+							//theme
+							el(SelectControl, {
+								label: __('Theme', 'novashare'),
+								options : [
+									{ label: __('Default (Accent Background)', 'novashare'), value: '' },
+									{ label: __('Simple (Transparent Background)', 'novashare'), value: 'simple' },
+									{ label: __('Simple Alternate (Gray Background)', 'novashare'), value: 'simple-alt' },
+								],
+								onChange: (value) => {
+									props.setAttributes({ theme: value });
+								},
+								value: props.attributes.theme
+							}),
+	 
+							//call to action text
+							el(TextControl, {
+								label: __('Call to Action Text', 'novashare'),
+								onChange: (value) => {
+									props.setAttributes({ cta_text: value });
+								},
+								value: props.attributes.cta_text
+							}),
+
+							//call to action position
+							el(SelectControl, {
+								label: __('Call to Action Position', 'novashare'),
+								options : [
+									{ label: __('Right (Default)', 'novashare'), value: '' },
+									{ label: __('Left', 'novashare'), value: 'left' },
+								],
+								onChange: (value) => {
+									props.setAttributes({ cta_position: value });
+								},
+								value: props.attributes.cta_position
+							}),
+	 
+							//remove url
+							el(ToggleControl, {
+								label: __('Remove URL', 'novashare'),
+								onChange: (value) => {
+									props.setAttributes({ remove_url: value });
+								},
+								checked: props.attributes.remove_url,
+							}),
+
+							//remove username
+							el(ToggleControl, {
+								label: __('Remove Username', 'novashare'),
+								onChange: (value) => {
+									props.setAttributes({ remove_username: value });
+								},
+								checked: props.attributes.remove_username,
+							}),
+
+							//hide hashtags
+							el(ToggleControl, {
+								label: __('Hide Hashtags', 'novashare'),
+								onChange: (value) => {
+									props.setAttributes({ hide_hashtags: value });
+								},
+								checked: props.attributes.hide_hashtags,
+							}),
+
+							//accent color
+							el(ColorPicker, {
+								label: __('Accent Color', 'novashare'),
+								onChangeComplete: (value) => {
+									if(value.source == 'hex' && value.hex == '') {
+										props.setAttributes({ accent_color: '' });
+									}
+									else {
+										props.setAttributes({ accent_color: value.hex });
+									}
+								},
+								color: props.attributes.accent_color && props.attributes.accent_color  !== '' ? props.attributes.accent_color : defaultAccentColor,
+								disableAlpha: true,
+							}),
+						)
+					),
+
+					//print block in editor
+					el('div', {
+						className: props.className + (() => {
+
+							var extra_classes = '';
+
+							extra_classes+= ' novashare-ctt';
+
+							//theme container class
+							if(props.attributes.theme) {
+								extra_classes+= ' novashare-ctt-' + props.attributes.theme;
+							} else if(novashare.click_to_tweet.theme) {
+								extra_classes+= ' novashare-ctt-' + novashare.click_to_tweet.theme;
+							}
+							
+							//cta position container class
+							if(props.attributes.cta_position) {
+								extra_classes+= ' novashare-ctt-cta-' + props.attributes.cta_position;
+							} else if(novashare.click_to_tweet.cta_position) {
+								extra_classes+= ' novashare-ctt-cta-' + novashare.click_to_tweet.cta_position;
+							}
+
+							return extra_classes;
+
+						})(), 
+						style: {
+							backgroundColor: (() => {
+								if(!props.attributes.theme && !novashare.click_to_tweet.theme) {
+									return props.attributes.accent_color;
+								}
+							})(),
+							borderColor: (() => {
+								return props.attributes.accent_color;
+							})(),
+						}			
+					},
+
+						//editable tweet
+						el('div', { className: 'novashare-ctt-tweet' },
+							el(RichText, {
+								format: 'string',
+								onChange: (value) => {
+									props.setAttributes({ tweet: value });
+								},
+								value: props.attributes.tweet,
+								allowedFormats: []
+							})
+						),
+
+						//tweet cta container
+						el('div', { className: 'novashare-ctt-cta-container' },
+							el('span', { className: 'novashare-ctt-cta',
+								style: {
+									color: (() => {
+										if(props.attributes.theme || novashare.click_to_tweet.theme) {
+											return props.attributes.accent_color;
+										}
+									})(),
+								}
+							},
+
+								//cta text
+								el('span', { className: 'novashare-ctt-cta-text' },
+									(() => {
+										if(props.attributes.cta_text) {
+											return props.attributes.cta_text;
+										} else if(novashare.click_to_tweet.cta_text) {
+											return novashare.click_to_tweet.cta_text;
+										} else {
+											return "Click to Tweet";
+										}
+									})()
+								),
+
+								//cta icon
+								el('span', { className: 'novashare-ctt-cta-icon' }, 
+									(() => {
+										return iconTwitter;
+									})()
+								)
+							)
+						)
+					),
+
+					//characters remaining
+					el('div', { className: 'novashare-ctt-char-count' },
+
+						//calculate character count
+						(() => {
+
+							//max character count
+							var initial_char_count = 280;
+
+							//calculate section lengths
+							var username_length = (novashare.twitter_username && props.attributes.remove_username != true) ? (novashare.twitter_username.length + 6) : 0;
+							var url_length = (props.permalink && props.attributes.remove_url != true) ? 24 : 0;
+							var tweet_length = (props.attributes.tweet) ? props.attributes.tweet.replace(/(<([^>]+)>)/ig, "").length : 0;
+							
+							//calculate remaining characters
+							chars_remaining = initial_char_count - username_length - url_length - tweet_length;
+
+							//container class based on +/- status
+							count_status = (chars_remaining >= 0) ? 'novashare-ctt-positive' : 'novashare-ctt-negative';
+						})(),
+
+						//print finished character count
+						el('span', { className: count_status }, 
+							(() => {
+								return chars_remaining + " " + __("Characters Remaining", 'novashare');
+							})()
+						)
+					)
+				)
+			);
+		}),
+
+		//no need to save anything since we are using PHP to render on the front end
+		save: function(props) {
+			return null;
+		}
+	});
+})(
+	window.wp.blocks,
+	window.wp.element,
+	window.wp.blockEditor,
+	window.wp.components,
+	window.wp.data
+);
