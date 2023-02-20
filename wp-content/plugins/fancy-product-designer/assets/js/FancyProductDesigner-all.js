b825701632c572ca700aadcd75cfcfea76f3d52e
hzdu <iado@foxmail.com>
2023-02-21 00:14:26 +0800

    update

--- wp-content/plugins/fancy-product-designer/assets/js/FancyProductDesigner-all.js
+++ wp-content/plugins/fancy-product-designer/assets/js/FancyProductDesigner-all.js
@@ -2406,7 +2406,9 @@ var FPDUtil =  {
 
 };
 
-var FPDEmojisRegex = /<|>|(?:[\u261D\u26F9\u270A-\u270D]|\uD83C[\uDF85\uDFC2-\uDFC4\uDFC7\uDFCA-\uDFCC]|\uD83D[\uDC42\uDC43\uDC46-\uDC50\uDC66-\uDC69\uDC6E\uDC70-\uDC78\uDC7C\uDC81-\uDC83\uDC85-\uDC87\uDCAA\uDD74\uDD75\uDD7A\uDD90\uDD95\uDD96\uDE45-\uDE47\uDE4B-\uDE4F\uDEA3\uDEB4-\uDEB6\uDEC0\uDECC]|\uD83E[\uDD18-\uDD1C\uDD1E\uDD1F\uDD26\uDD30-\uDD39\uDD3D\uDD3E\uDDD1-\uDDDD])(?:\uD83C[\uDFFB-\uDFFF])?|(?:[\u231A\u231B\u23E9-\u23EC\u23F0\u23F3\u25FD\u25FE\u2614\u2615\u2648-\u2653\u267F\u2693\u26A1\u26AA\u26AB\u26BD\u26BE\u26C4\u26C5\u26CE\u26D4\u26EA\u26F2\u26F3\u26F5\u26FA\u26FD\u2705\u270A\u270B\u2728\u274C\u274E\u2753-\u2755\u2757\u2795-\u2797\u27B0\u27BF\u2B1B\u2B1C\u2B50\u2B55]|\uD83C[\uDC04\uDCCF\uDD8E\uDD91-\uDD9A\uDDE6-\uDDFF\uDE01\uDE1A\uDE2F\uDE32-\uDE36\uDE38-\uDE3A\uDE50\uDE51\uDF00-\uDF20\uDF2D-\uDF35\uDF37-\uDF7C\uDF7E-\uDF93\uDFA0-\uDFCA\uDFCF-\uDFD3\uDFE0-\uDFF0\uDFF4\uDFF8-\uDFFF]|\uD83D[\uDC00-\uDC3E\uDC40\uDC42-\uDCFC\uDCFF-\uDD3D\uDD4B-\uDD4E\uDD50-\uDD67\uDD7A\uDD95\uDD96\uDDA4\uDDFB-\uDE4F\uDE80-\uDEC5\uDECC\uDED0-\uDED2\uDEEB\uDEEC\uDEF4-\uDEF8]|\uD83E[\uDD10-\uDD3A\uDD3C-\uDD3E\uDD40-\uDD45\uDD47-\uDD4C\uDD50-\uDD6B\uDD80-\uDD97\uDDC0\uDDD0-\uDDE6])|(?:[#\*0-9\xA9\xAE\u203C\u2049\u2122\u2139\u2194-\u2199\u21A9\u21AA\u231A\u231B\u2328\u23CF\u23E9-\u23F3\u23F8-\u23FA\u24C2\u25AA\u25AB\u25B6\u25C0\u25FB-\u25FE\u2600-\u2604\u260E\u2611\u2614\u2615\u2618\u261D\u2620\u2622\u2623\u2626\u262A\u262E\u262F\u2638-\u263A\u2640\u2642\u2648-\u2653\u2660\u2663\u2665\u2666\u2668\u267B\u267F\u2692-\u2697\u2699\u269B\u269C\u26A0\u26A1\u26AA\u26AB\u26B0\u26B1\u26BD\u26BE\u26C4\u26C5\u26C8\u26CE\u26CF\u26D1\u26D3\u26D4\u26E9\u26EA\u26F0-\u26F5\u26F7-\u26FA\u26FD\u2702\u2705\u2708-\u270D\u270F\u2712\u2714\u2716\u271D\u2721\u2728\u2733\u2734\u2744\u2747\u274C\u274E\u2753-\u2755\u2757\u2763\u2764\u2795-\u2797\u27A1\u27B0\u27BF\u2934\u2935\u2B05-\u2B07\u2B1B\u2B1C\u2B50\u2B55\u3030\u303D\u3297\u3299]|\uD83C[\uDC04\uDCCF\uDD70\uDD71\uDD7E\uDD7F\uDD8E\uDD91-\uDD9A\uDDE6-\uDDFF\uDE01\uDE02\uDE1A\uDE2F\uDE32-\uDE3A\uDE50\uDE51\uDF00-\uDF21\uDF24-\uDF93\uDF96\uDF97\uDF99-\uDF9B\uDF9E-\uDFF0\uDFF3-\uDFF5\uDFF7-\uDFFF]|\uD83D[\uDC00-\uDCFD\uDCFF-\uDD3D\uDD49-\uDD4E\uDD50-\uDD67\uDD6F\uDD70\uDD73-\uDD7A\uDD87\uDD8A-\uDD8D\uDD90\uDD95\uDD96\uDDA4\uDDA5\uDDA8\uDDB1\uDDB2\uDDBC\uDDC2-\uDDC4\uDDD1-\uDDD3\uDDDC-\uDDDE\uDDE1\uDDE3\uDDE8\uDDEF\uDDF3\uDDFA-\uDE4F\uDE80-\uDEC5\uDECB-\uDED2\uDEE0-\uDEE5\uDEE9\uDEEB\uDEEC\uDEF0\uDEF3-\uDEF8]|\uD83E[\uDD10-\uDD3A\uDD3C-\uDD3E\uDD40-\uDD45\uDD47-\uDD4C\uDD50-\uDD6B\uDD80-\uDD97\uDDC0\uDDD0-\uDDE6])\uFE0F/g;
+var FPDDisallowChars = /<|>/g;
+
+var FPDEmojisRegex = /(?:[\u261D\u26F9\u270A-\u270D]|\uD83C[\uDF85\uDFC2-\uDFC4\uDFC7\uDFCA-\uDFCC]|\uD83D[\uDC42\uDC43\uDC46-\uDC50\uDC66-\uDC69\uDC6E\uDC70-\uDC78\uDC7C\uDC81-\uDC83\uDC85-\uDC87\uDCAA\uDD74\uDD75\uDD7A\uDD90\uDD95\uDD96\uDE45-\uDE47\uDE4B-\uDE4F\uDEA3\uDEB4-\uDEB6\uDEC0\uDECC]|\uD83E[\uDD18-\uDD1C\uDD1E\uDD1F\uDD26\uDD30-\uDD39\uDD3D\uDD3E\uDDD1-\uDDDD])(?:\uD83C[\uDFFB-\uDFFF])?|(?:[\u231A\u231B\u23E9-\u23EC\u23F0\u23F3\u25FD\u25FE\u2614\u2615\u2648-\u2653\u267F\u2693\u26A1\u26AA\u26AB\u26BD\u26BE\u26C4\u26C5\u26CE\u26D4\u26EA\u26F2\u26F3\u26F5\u26FA\u26FD\u2705\u270A\u270B\u2728\u274C\u274E\u2753-\u2755\u2757\u2795-\u2797\u27B0\u27BF\u2B1B\u2B1C\u2B50\u2B55]|\uD83C[\uDC04\uDCCF\uDD8E\uDD91-\uDD9A\uDDE6-\uDDFF\uDE01\uDE1A\uDE2F\uDE32-\uDE36\uDE38-\uDE3A\uDE50\uDE51\uDF00-\uDF20\uDF2D-\uDF35\uDF37-\uDF7C\uDF7E-\uDF93\uDFA0-\uDFCA\uDFCF-\uDFD3\uDFE0-\uDFF0\uDFF4\uDFF8-\uDFFF]|\uD83D[\uDC00-\uDC3E\uDC40\uDC42-\uDCFC\uDCFF-\uDD3D\uDD4B-\uDD4E\uDD50-\uDD67\uDD7A\uDD95\uDD96\uDDA4\uDDFB-\uDE4F\uDE80-\uDEC5\uDECC\uDED0-\uDED2\uDEEB\uDEEC\uDEF4-\uDEF8]|\uD83E[\uDD10-\uDD3A\uDD3C-\uDD3E\uDD40-\uDD45\uDD47-\uDD4C\uDD50-\uDD6B\uDD80-\uDD97\uDDC0\uDDD0-\uDDE6])|(?:[#\*0-9\xA9\xAE\u203C\u2049\u2122\u2139\u2194-\u2199\u21A9\u21AA\u231A\u231B\u2328\u23CF\u23E9-\u23F3\u23F8-\u23FA\u24C2\u25AA\u25AB\u25B6\u25C0\u25FB-\u25FE\u2600-\u2604\u260E\u2611\u2614\u2615\u2618\u261D\u2620\u2622\u2623\u2626\u262A\u262E\u262F\u2638-\u263A\u2640\u2642\u2648-\u2653\u2660\u2663\u2665\u2666\u2668\u267B\u267F\u2692-\u2697\u2699\u269B\u269C\u26A0\u26A1\u26AA\u26AB\u26B0\u26B1\u26BD\u26BE\u26C4\u26C5\u26C8\u26CE\u26CF\u26D1\u26D3\u26D4\u26E9\u26EA\u26F0-\u26F5\u26F7-\u26FA\u26FD\u2702\u2705\u2708-\u270D\u270F\u2712\u2714\u2716\u271D\u2721\u2728\u2733\u2734\u2744\u2747\u274C\u274E\u2753-\u2755\u2757\u2763\u2764\u2795-\u2797\u27A1\u27B0\u27BF\u2934\u2935\u2B05-\u2B07\u2B1B\u2B1C\u2B50\u2B55\u3030\u303D\u3297\u3299]|\uD83C[\uDC04\uDCCF\uDD70\uDD71\uDD7E\uDD7F\uDD8E\uDD91-\uDD9A\uDDE6-\uDDFF\uDE01\uDE02\uDE1A\uDE2F\uDE32-\uDE3A\uDE50\uDE51\uDF00-\uDF21\uDF24-\uDF93\uDF96\uDF97\uDF99-\uDF9B\uDF9E-\uDFF0\uDFF3-\uDFF5\uDFF7-\uDFFF]|\uD83D[\uDC00-\uDCFD\uDCFF-\uDD3D\uDD49-\uDD4E\uDD50-\uDD67\uDD6F\uDD70\uDD73-\uDD7A\uDD87\uDD8A-\uDD8D\uDD90\uDD95\uDD96\uDDA4\uDDA5\uDDA8\uDDB1\uDDB2\uDDBC\uDDC2-\uDDC4\uDDD1-\uDDD3\uDDDC-\uDDDE\uDDE1\uDDE3\uDDE8\uDDEF\uDDF3\uDDFA-\uDE4F\uDE80-\uDEC5\uDECB-\uDED2\uDEE0-\uDEE5\uDEE9\uDEEB\uDEEC\uDEF0\uDEF3-\uDEF8]|\uD83E[\uDD10-\uDD3A\uDD3C-\uDD3E\uDD40-\uDD45\uDD47-\uDD4C\uDD50-\uDD6B\uDD80-\uDD97\uDDC0\uDDD0-\uDDE6])\uFE0F/g;
 
 /**
  * The class defining the default options for Fancy Product Designer.
@@ -5850,6 +5852,7 @@ var FancyProductDesignerView = function($productStage, view, callback, fabricCan
 		if(type === 'text') {
 			//strip HTML tags
 			source = source.replace(/(<([^>]+)>)/ig,"");
+            source = source.replace(FPDDisallowChars, '');
 			title = title.replace(/(<([^>]+)>)/ig,"");
 		}
 
@@ -6596,6 +6599,8 @@ var FancyProductDesignerView = function($productStage, view, callback, fabricCan
 		if(typeof parameters.text === 'string') {
 
 			var text = parameters.text;
+            
+            text = text.replace(FPDDisallowChars, '');
 
 			//remove emojis
 			if(instance.options.disableTextEmojis) {
@@ -9284,7 +9289,7 @@ var FPDToolbarSmart = function($uiElementToolbar, fpdInstance) {
 	};
 
 	this.toggle = function(showHide, reset) {
-
+        
 		reset = reset === undefined ? true : reset;
 
 		if(!showHide && reset) {
@@ -9293,6 +9298,7 @@ var FPDToolbarSmart = function($uiElementToolbar, fpdInstance) {
 
 		showHide = $uiElementToolbar.find('.fpd-top-tools > div:visible, .fpd-bottom-tools > div:visible').length == 0 ? false : showHide;
 		$uiElementToolbar.toggleClass('fpd-show', showHide);
+        $('body,html').toggleClass('fpd-toolbar-visible', showHide);
 
 	};
 
@@ -9579,11 +9585,11 @@ var FPDMainBar = function(fpdInstance, $mainBar, $modules, $draggableDialog) {
 		var $selectedNavItem = $nav.children('div').removeClass('fpd-active')
 		.filter(dynamicDesignsId ? '[data-dynamic-designs-id="'+dynamicDesignsId+'"]' : '[data-module="'+name+'"]:not([data-dynamic-designs-id])')
 		.addClass('fpd-active');
-
+        
 		instance.$selectedModule = instance.$content.children('div').removeClass('fpd-active')
 		.filter(dynamicDesignsId ? '[data-dynamic-designs-id="'+dynamicDesignsId+'"]' : '[data-module="'+name+'"]:not([data-dynamic-designs-id])')
 		.addClass('fpd-active');
-
+        
 		if(fpdInstance.mainOptions.uiTheme !== 'doyle' && instance.$content.parent('.fpd-draggable-dialog').length > 0) {
 
 			if($draggableDialog.attr('style') === undefined || $draggableDialog.attr('style') === '') {
@@ -9632,7 +9638,7 @@ var FPDMainBar = function(fpdInstance, $mainBar, $modules, $draggableDialog) {
 	};
 
 	this.callSecondary = function(className) {
-
+        
 		instance.callModule('secondary');
 
 		instance.$content.children('.fpd-secondary-module').children('.'+className).addClass('fpd-active')
@@ -9753,7 +9759,7 @@ var FPDMainBar = function(fpdInstance, $mainBar, $modules, $draggableDialog) {
 	};
 
 	this.toggleUploadZonePanel = function(toggle) {
-
+                
 		toggle = typeof toggle === 'undefined' ? true : toggle;
 
 		//do nothing when custom image is loading
@@ -11285,7 +11291,7 @@ var FPDImageEditor = function($container, targetElement, fpdInstance) {
 						targetElement.setCoords();
 						targetElement.canvas.renderAll();
 
-					});
+					}, {crossOrigin:'anonymous'});
 
 				}
 
@@ -12133,13 +12139,15 @@ var FPDTextModule = function(fpdInstance, $module) {
 			var text = this.value,
 				maxLength = currentViewOptions ? currentViewOptions.customTextParameters.maxLength : 0,
 				maxLines = currentViewOptions ? currentViewOptions.customTextParameters.maxLines : 0;
-
+            
+            text = text.replace(FPDDisallowChars, '');
+            
 			//remove emojis
 			if(fpdInstance.mainOptions.disableTextEmojis) {
 				text = text.replace(FPDEmojisRegex, '');
 				text = text.replace(String.fromCharCode(65039), ""); //fix: some emojis left a symbol with char code 65039
 			}
-
+            
 			if(maxLength != 0 && text.length > maxLength) {
 				text = text.substr(0, maxLength);
 			}
@@ -12147,7 +12155,7 @@ var FPDTextModule = function(fpdInstance, $module) {
 			if(maxLines != 0 && text.split("\n").length > maxLines) {
 				text = text.replace(/([\s\S]*)\n/, "$1");
 			}
-
+            
 			this.value = text;
 
 		});
@@ -12594,7 +12602,9 @@ var FPDLayersModule = {
 
 			var $this = $(this),
 				element = $this.parents('.fpd-list-row').data('element');
-
+            
+            this.value = this.value.replace(FPDDisallowChars, '');
+            
 			//remove emojis
 			if(fpdInstance.mainOptions.disableTextEmojis) {
 				this.value = this.value.replace(FPDEmojisRegex, '');
@@ -14059,13 +14069,25 @@ var FPDTextLayersModule = {
 			evt.stopPropagation();
 
 			var $this = $(this),
-				element = $this.parents('.fpd-text-layer-item').data('element');
-
+				element = $this.parents('.fpd-text-layer-item').data('element'),
+                maxLength = element.maxLength,
+                maxLines = element.maxLines;
+                            
+            this.value = this.value.replace(FPDDisallowChars, '');
+            
 			//remove emojis
 			if(fpdInstance.mainOptions.disableTextEmojis) {
 				this.value = this.value.replace(FPDEmojisRegex, '');
 				this.value = this.value.replace(String.fromCharCode(65039), ""); //fix: some emojis left a symbol with char code 65039
 			}
+            
+            if(maxLength != 0 && this.value.length > maxLength) {
+                this.value = this.value.substr(0, maxLength);
+            }
+            
+            if(maxLines != 0 && this.value.split("\n").length > maxLines) {
+                this.value = this.value.replace(/([\s\S]*)\n/, "$1");
+            }
 
 			fpdInstance.currentViewInstance.setElementParameters({text: this.value}, element);
 
@@ -14289,7 +14311,7 @@ var FPDToolbarSide = function($uiElementToolbar, fpdInstance) {
 	this.placement = fpdInstance.mainOptions.toolbarPlacement;
 
 	var _initialize = function() {
-
+        
 		$uiElementToolbar.addClass('fpd-ui-theme-'+fpdInstance.mainOptions.uiTheme)
 
 		instance.setPlacement(fpdInstance.mainOptions.toolbarPlacement);
@@ -14652,7 +14674,7 @@ var FPDToolbarSide = function($uiElementToolbar, fpdInstance) {
 					});
 
 				}
-
+                
 				instance.toggle();
 				$uiToolbarSub.find('.fpd-slider-number').change();
 				FPDUtil.createScrollbar($uiToolbarSub.find('.fpd-active .fpd-scroll-area'));
@@ -15292,7 +15314,7 @@ var FPDToolbarSide = function($uiElementToolbar, fpdInstance) {
 		$uiToolbarSub.find('.fpd-panel-tabs-content, .fpd-panel-tabs').children().removeClass('fpd-active')
 
 		$colorPicker.addClass('fpd-hidden');
-
+        
 	};
 
 	this.updatePosition = function(element, showHide) {
@@ -15300,7 +15322,7 @@ var FPDToolbarSide = function($uiElementToolbar, fpdInstance) {
 	};
 
 	this.toggle = function(showHide, reset) {
-
+        
 		showHide = showHide === undefined ? true : showHide;
 		reset = reset === undefined ? true : reset;
 
@@ -15309,6 +15331,7 @@ var FPDToolbarSide = function($uiElementToolbar, fpdInstance) {
 		}
 
 		$uiElementToolbar.toggleClass('fpd-show', showHide);
+        $('body,html').toggleClass('fpd-toolbar-visible', showHide);
 
 	};
 
@@ -15935,13 +15958,17 @@ var FancyProductDesigner = function(elem, opts) {
 		});
 
 		//window resize handler
-		var device = '';
-		$window.resize(function() {
+		var device = '',
+            currentWindowWidth = 0;
 
+		$window.resize(function() {
+                        
 			//fix for android browser, because keyboard trigger resize event
-			if(inTextField) {
+			if(window.innerWidth === currentWindowWidth || inTextField) {
 				return;
 			}
+            
+            currentWindowWidth = window.innerWidth;
 
 			if(instance.currentViewInstance) {
 				instance.currentViewInstance.resetCanvasSize();
@@ -15960,7 +15987,7 @@ var FancyProductDesigner = function(elem, opts) {
 				}
 
 			}
-
+            
 			//deselect element if one is selected and active element is not input (FB browser fix)
 			//alert(document.activeElement);
 			if(instance.currentElement && $(document.activeElement).is(':not(input)') && $(document.activeElement).is(':not(textarea)')) {
@@ -16126,7 +16153,7 @@ var FancyProductDesigner = function(elem, opts) {
 
 			evt.stopPropagation();
 			evt.preventDefault();
-
+            
 			if(element && !element._ignore && instance.currentViewInstance) {
 
 				//upload zone is selected
@@ -16142,29 +16169,24 @@ var FancyProductDesigner = function(elem, opts) {
 
 					//mobile fix: elementSelect is triggered before click, this was adding an image on mobile
 					setTimeout(function() {
-
 						instance.currentViewInstance.currentUploadZone = element.title;
 						instance.mainBar.toggleUploadZoneAdds(customAdds);
 						instance.mainBar.toggleUploadZonePanel();
-
 					}, 100);
-
 
 					return;
 				}
 				//if element has no upload zone and an upload zone is selected, close dialogs and call first module
 				else if(instance.currentViewInstance.currentUploadZone) {
-
+                    
 					instance.mainBar.toggleDialog(false);
 					instance.mainBar.toggleUploadZonePanel(false);
 
-
 				}
-
+                
 				instance.toolbar.update(element);
 
 				if(instance.mainOptions.openTextInputOnSelect && FPDUtil.getType(element.type) === 'text' && element.editable) {
-
 					$elementToolbar.find('.fpd-tool-edit-text:first').click();
 				}
 
@@ -16172,7 +16194,7 @@ var FancyProductDesigner = function(elem, opts) {
 
 			}
 			else {
-
+                
 				instance.toolbar.toggle(false);
 				$body.children('[class^="fpd-element-toolbar"]').find('input').spectrum('destroy');
 
@@ -16523,7 +16545,7 @@ var FancyProductDesigner = function(elem, opts) {
 				   instance.deselectElement();
 
 			}
-
+            
 			//close upload zone panel if click outside of fpd-container, needed otherwise elements can be added to upload zone e.g. mspc
 			if($target.closest('.fpd-container, .fpd-modal-internal').length === 0
 				&& instance.currentViewInstance && instance.currentViewInstance.currentUploadZone
